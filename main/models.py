9faa706f46f38df87db7b1ce00c00043e8fe4e93
mhelmialf <alfarissimhelmi@gmail.com>
2025-09-16 18:38:50 +0700

    Fixing Rendering Template

--- main/models.py
+++ main/models.py
@@ -10,6 +10,7 @@ class Product(models.Model):
         ('training', 'Training & Fitness'),      # cone, mini goal, speed ladder
     ]
     
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     product_name = models.CharField(max_length=50)
     price = models.IntegerField()
     stock = models.IntegerField()
@@ -20,3 +21,11 @@ class Product(models.Model):
     
     def __str__(self):
         return self.title
+    
+    @property
+    def is_product_hot(self):
+        return self.product_views > 20
+        
+    def increment_views(self):
+        self.product_views += 1
+        self.save()
